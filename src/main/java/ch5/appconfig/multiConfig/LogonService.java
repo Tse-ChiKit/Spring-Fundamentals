cae53108716845e5cc4b39c2800e84bd2c520ffa
Eric Xie <397706572@qq.com>
2020-01-18 23:01:02 +0800

    test rewrite git email

--- src/main/java/ch5/appconfig/multiConfig/LogonService.java
+++ src/main/java/ch5/appconfig/multiConfig/LogonService.java
@@ -5,23 +5,27 @@ import commons.UserDao;
 
 public class LogonService {
 
-	private LogDao logDao;
-	private UserDao userDao;
-	public LogDao getLogDao() {
-		return logDao;
-	}
-	public void setLogDao(LogDao logDao) {
-		this.logDao = logDao;
-	}
-	public UserDao getUserDao() {
-		return userDao;
-	}
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
-	
-	public void printHelllo(){
-		System.out.println("hello test new git emailgit!");
-	}
-	
+    private LogDao logDao;
+    private UserDao userDao;
+
+    public LogDao getLogDao() {
+        return logDao;
+    }
+
+    public void setLogDao(LogDao logDao) {
+        this.logDao = logDao;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void printHelllo() {
+        System.out.println("hello test rewrite emails!");
+    }
+
 }
